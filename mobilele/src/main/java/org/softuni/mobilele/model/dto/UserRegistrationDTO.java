f27f0158d581bc508725d4dbbd9234592fe14e1f
Petar Borisov <108021054+PetarBorisov@users.noreply.github.com>
2023-09-21 21:02:15 +0300

    Delete mobilele directory

--- mobilele/src/main/java/org/softuni/mobilele/model/dto/UserRegistrationDTO.java
+++
@@ -1,8 +0,0 @@
-package org.softuni.mobilele.model.dto;
-
-public record UserRegistrationDTO(String firstName,
-                                  String lastName,
-                                  String email,
-                                  String password,
-                                  String confirmPassword) {
-}
