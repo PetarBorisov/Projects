36e649e3818edaa1a269909bfc399f35697562ad
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-10-14 01:29:26 +0300

    Update ModelRepository.java

--- Mobilelele/src/main/java/org/softuni/mobilele/repository/ModelRepository.java
+++ Mobilelele/src/main/java/org/softuni/mobilele/repository/ModelRepository.java
@@ -1,4 +1,13 @@
 package org.softuni.mobilele.repository;
 
-public interface ModelRepository {
+import org.softuni.mobilele.model.entity.ModelEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ModelRepository extends JpaRepository<ModelEntity, Long> {
+
+    List<ModelEntity> findAllByBrandId (Long id);
 }
