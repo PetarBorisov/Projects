89402fe5eb80b38e7418828562f4840947bcc31b
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-10-13 18:33:43 +0300

    Update UserEntity.java

--- Mobilelele/src/main/java/org/softuni/mobilele/model/entity/UserEntity.java
+++ Mobilelele/src/main/java/org/softuni/mobilele/model/entity/UserEntity.java
@@ -1,5 +1,6 @@
 package org.softuni.mobilele.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
@@ -7,6 +8,7 @@ import jakarta.persistence.Table;
 @Table(name = "users")
 public class UserEntity extends BaseEntity{
 
+    @Column(unique = true)
     private String email;
 
     private String password;
