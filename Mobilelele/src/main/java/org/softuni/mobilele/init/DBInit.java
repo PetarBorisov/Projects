3c03ba9b72118b65d517185ecbc143a7c9647679
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-10-30 15:34:03 +0200

    Spring_Securirty_using

--- Mobilelele/src/main/java/org/softuni/mobilele/init/DBInit.java
+++ Mobilelele/src/main/java/org/softuni/mobilele/init/DBInit.java
@@ -5,11 +5,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DBInit implements CommandLineRunner{
+public class DBInit implements CommandLineRunner {
 
     private final String defaultAdminPass;
 
-    public DBInit(@Value("${default.admin.pass}") String defaultAdminPass) {
+    public DBInit(@Value("${mobilele.default.admin.pass}") String defaultAdminPass) {
         this.defaultAdminPass = defaultAdminPass;
     }
 
